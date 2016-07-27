e36387b05d7163f644ea38a9e922421d2c4d362b
Paul Kremer <pkremer@spurious.biz>
2016-07-27 12:10:40 +0200

    add initialization script for empty databases

---
+++ initneo4j.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# Quick hack to run an automatic import of graphml file(s) on startup, if this is a fresh instance
+
+# docker run -v graphmls/:/initneo4j/ --rm infothrill/neo4j-shell-tools:2.3 /initneo4j.sh
+
+if [ ! -d data/graph.db ];
+then
+    if [ -d /initneo4j/ ];
+    then
+    	#ls /initneo4j/
+        shopt -s nullglob
+        for f in /initneo4j/*.graphml;
+        do
+        	./bin/neo4j-shell -path ./data -c "import-graphml -i ${f} -t"
+        done
+    fi
+fi
+exec /docker-entrypoint.sh $@
